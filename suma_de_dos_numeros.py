e8811998143fc1f0f5684fc3166e1a5c74e4f5f0
Joseph Luis García Escobar <josephgarciaescobar597@gmail.com>
2025-04-01 23:09:53 -0500

    pilote de suma de dos numeros

---
+++ suma_de_dos_numeros.py
@@ -0,0 +1,12 @@
+
+numero1 = input("ingrese un numero: ")
+numero1 = float(numero1)
+
+numero2 = input("ingrese un numero: ")
+numero2 = float(numero2)
+
+suma = numero1 + numero2
+
+
+print(suma)
+
